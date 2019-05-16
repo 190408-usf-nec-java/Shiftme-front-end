0c4227e42a9081e0e0f5170092c699afc1c78ae8
HowardDennis <h2dennis@gmail.com>
2019-05-16 14:26:13 -0400

    Only a REPRESENTATIVE. Changable and will be done in Java

---
+++ Database Entity Representative/ShiftMe-SQL Script.sql
@@ -0,0 +1,47 @@
+create table if not exists users (
+	id serial primary key,
+	role integer not null,
+	email varchar(150) not null,
+	fname varchar(100) not null,
+	lname varchar(100) not null
+);
+
+create table if not exists credentials (
+	id serial primary key,
+	username varchar(50) not null,
+	hashed_password varchar(150) not null,
+	user_id integer not null references users(id) on delete cascade
+);
+
+create table if not exists weeks (
+	id serial primary key
+);
+
+create table if not exists days (
+	id serial primary key,
+	name varchar(9) not null
+);
+
+create table if not exists shifts (
+	id serial primary key,
+	startTime timestamptz not null,
+	endTime timestamptz not null
+);
+
+create table if not exists week_to_day_junction (
+	id serial primary key,
+	week_id integer not null references weeks(id) on delete cascade,
+	day_id integer not null references days(id)
+);
+
+create table if not exists day_to_shift_junction (
+	id serial primary key,
+	day_id integer not null references days(id),
+	shift_id integer not null references shifts(id) on delete cascade
+);
+
+create table if not exists user_to_shift_junction (
+	id serial primary key,
+	user_id integer not null references users(id) on delete cascade,
+	shift_id integer not null references shifts(id) on delete cascade
+);
