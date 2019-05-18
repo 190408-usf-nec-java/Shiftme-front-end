b5943afafe86f92cb7c04a1bb5f17f23b2b9cb7d
DevinStuart <onefunnystuart@gmail.com>
2019-05-18 14:34:06 -0400

    Shifts component buttons and shift view
    Added previous and next week buttons and functionality
    added timestamp to side of page and logic for displaying shift length

--- src/app/components/shifts/shifts.component.ts
+++ src/app/components/shifts/shifts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Week } from 'src/app/classes/week';
 import { ShiftService } from 'src/app/services/shift.service';
 import { User } from 'src/app/classes/user';
@@ -14,20 +14,69 @@ import { Day } from 'src/app/classes/day';
 export class ShiftsComponent implements OnInit {
 
   currentWeek: Week;
+  currentDay = 0;
+  currentDate;
+
   constructor(private shiftService: ShiftService) { }
 
   ngOnInit() {
-    let today = new Date(Date.now());
-    let date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear()
+    const today = new Date(Date.now());
+    const date = this.formartToUsableDate(today); //(today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear();
     console.log(date);
-    this.shiftService.fetchCurrentWeek(date);
-    // this.currentWeek = this.shiftService.getCurrentWeek();
+    // this.shiftService.fetchCurrentWeekByDate(date);
+    // this.currentWeek = this.shiftService.getCurrentWeek(); commented out until backend is working
     this.currentWeek = this.genSampleData();
+    this.currentDate = date;
 
   }
-
-
-
+  isSmallScreen() {
+    return window.innerWidth < 768;
+  }
+  isCurrentDay(day: number) {
+    if (this.isSmallScreen()) {
+      return day === this.currentDay;
+    } else {
+      return true;
+    }
+  }
+  next() {
+    if (this.isSmallScreen()) {
+      if (this.currentDay === 6){
+        this.shiftService.gotoNextWeek();
+        this.currentWeek = this.shiftService.getCurrentWeek();
+        this.currentDay = 0;
+      } else {
+        this.currentDay++;
+      }
+    } else {
+      this.shiftService.gotoNextWeek();
+      this.currentWeek = this.shiftService.getCurrentWeek();
+    }
+  }
+  previous() {
+    if (this.isSmallScreen()) {
+      if (this.currentDay === 0) { // if we're on the first day of the week, get previous week;
+        this.shiftService.gotoPreviousWeek();
+        this.currentWeek = this.shiftService.getCurrentWeek();
+        this.currentDay = 6;
+      } else {
+        this.currentDay--;
+      }
+    } else {
+      this.shiftService.gotoPreviousWeek();
+      this.currentWeek = this.shiftService.getCurrentWeek();
+    }
+  }
+  formartToUsableDate(date: Date): string {
+    const today = new Date(date);
+    return (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear();
+  }
+  getHeight(shift: Shift): string {
+    const height = ((shift.endTime - shift.startTime) / 24) * 100; // calculate percentage of day the shift is
+    const hPercent = height + '%';
+    return hPercent;
+  }
+  // 768 is small breakpoint for bootstrap
 
 
 
@@ -39,9 +88,10 @@ export class ShiftsComponent implements OnInit {
     const martha = new User('Martha', 'Stuart', 'martha@margo.wiz', 'employee', 2,
       new Credentials('cookingiscool', 'aosihgoisahdpgoihaspdoigh', 'marthathecook'));
     const emps = new Array<User>(martha, bob);
-    const fShift = new Shift(8, 12, emps);
-    const sshift = new Shift(12, 4, emps);
-    const shifts = new Array<Shift>(fShift, sshift);
+    //const fShift = new Shift(8, 12, emps);
+    //const sshift = new Shift(12, 16, emps);
+    const tshift = new Shift(0, 24, emps);
+    const shifts = new Array<Shift>(tshift);
     const monday = new Day(new Date('5/6/2019'), shifts);
     const tuesday = new Day(new Date('5/7/2019'), shifts);
     const wednesday = new Day(new Date('5/8/2019'), shifts);
--- src/app/services/shift.service.ts
+++ src/app/services/shift.service.ts
@@ -8,19 +8,23 @@ import { map } from 'rxjs/internal/operators/map';
 })
 export class ShiftService {
   private currentWeek: Week;
+  private nextWeek: Week;
+  private previousWeek: Week;
   constructor(private httpClient: HttpClient) { }
-  fetchNextWeek(prevWeek: Week): void {
-    this.httpClient.get(`http://localhost:8080/week/${prevWeek.id}`, {
+
+  private fetchNextWeek(currentWeek: Week): void {
+    this.httpClient.get(`http://localhost:8080/week/${currentWeek.startDate}`, {
       observe: 'response',
     }).pipe(map(response => response.body as Week))
     .subscribe(response => {
       // this.loginStatusSubject.next(200);
-      this.currentWeek = response;
+      this.nextWeek = response;
     }, err => {
       // this.loginStatusSubject.next(err.status);
     });
   }
-  fetchCurrentWeek(date: string) {
+  // This method is only run on component init
+  public fetchCurrentWeekByDate(date: string) {
     this.httpClient.get(`http://localhost:8080/week/${date}`, {
       observe: 'response',
     }).pipe(map(response => response.body as Week))
@@ -31,7 +35,30 @@ export class ShiftService {
       // this.loginStatusSubject.next(err.status);
     });
   }
-  getCurrentWeek(): Week{
+  private fetchPreviousWeek(currentWeek: Week) {
+    this.httpClient.get(`http://localhost:8080/week/${currentWeek.startDate}`, {
+      observe: 'response',
+    }).pipe(map(response => response.body as Week))
+    .subscribe(response => {
+      // this.loginStatusSubject.next(200);
+      this.previousWeek = response;
+    }, err => {
+      // this.loginStatusSubject.next(err.status);
+    });
+  }
+  public getCurrentWeek(): Week{
     return this.currentWeek;
+  }
+  // Iterates through saved weeks forwards
+  public gotoNextWeek(): void {
+    this.previousWeek = this.currentWeek;
+    this.currentWeek = this.nextWeek;
+    this.fetchNextWeek(this.currentWeek); // this method sets nextweek
+  }
+  // Iterates through saved weeks backwords
+  public gotoPreviousWeek(): void {
+    this.nextWeek = this.currentWeek;
+    this.currentWeek = this.previousWeek;
+    this.fetchPreviousWeek(this.currentWeek); // this method sets previous week;
   }
 }
